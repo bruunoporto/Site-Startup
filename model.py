fcb0210b183e0117fb55e32832398e2df458b0a7
Pedro  Teixeira <pedroteixeir@me.com>
2021-05-06 02:34:34 -0300

    Changed Model from .txt to sqlite3

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class usuario:
     def __init__(self, senha, nome, idade, documento, cidade, bairro):
         self.__nome = nome
@@ -9,9 +11,11 @@ class usuario:
        
     
     def registro_usuario(self):
-        registro_de_usuarios = self.__nome + " - " + self.__senha  + "\n"
-        arquivo = open("registro_de_usuarios.txt","a")
-        arquivo.writelines(registro_de_usuarios)
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
+        cur.execute("INSERT INTO enterprises VALUES ('"+self.__nome+"','"+self.__idade+"','"+self.__senha+"','"+self.__cidade+"','"+self.__bairro+"','"+self.__documento+"')")
+        con.commit()
+        con.close()
 
 
 class empresa:
@@ -26,16 +30,47 @@ class empresa:
       
 
     def registro_empresa(self):
-        registro_de_empresas = self.__nome_empresa + " - " + self.__senha  + "\n"
-        arquivo = open("registro_de_empresas.txt","a")
-        arquivo.writelines(registro_de_empresas)
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
+        cur.execute("INSERT INTO enterprises VALUES ('"+self.__nome_empresa+"','"+self.__tipo_empresa+"','"+self.__senha+"','"+self.__cidade_empresa+"','"+self.__bairro_empresa+"','"+self.__rua_empresa+"','"+self.__numero_empresa+"')")
+        con.commit()
+        con.close()
 
 def login_model(nome,senha):
-    empresa = open("registro_de_empresas.txt","r")
-    usuario = open("registro_de_usuarios.txt","r")
-    login = nome + " - " + senha + "\n"
-    if login in empresa.readlines():
-        return (True, "Empresa")
-    elif login in usuario.readlines():
-        return (True, "Usuario")
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    for el in cur.execute("SELECT * FROM users"):
+        if el[0] == nome:
+            if el[2]== senha:
+                con.commit()
+                con.close()
+                return (True, "Usuario")
+    print("Not User")
+    for el in cur.execute("SELECT * FROM enterprises"):
+        print(el)
+        if el[0] == nome:
+            print("opa")
+            if el[2]== senha:
+                con.commit()
+                con.close()
+                return (True, "Empresa")
+    con.commit()
+    con.close()
     return (False, "")
+
+# con = sqlite3.connect('database.db')
+# cur = con.cursor()
+
+# Create table
+# cur.execute('''CREATE TABLE enterprises (name text, type text, password text, city text, neighborhood text, street text, number real)''')
+# cur.execute('''CREATE TABLE users (name text, age real, password text, city text, neighborhood text, document real)''')
+
+# Insert a row of data
+# cur.execute("INSERT INTO users VALUES ('Pedro Henrique',18,'12345678','Rio de Janeiro','Leblon','17120554700')")
+# cur.execute("INSERT INTO enterprises VALUES ('Beco dos fodao','Boca de fumo','12345678','Rio de Janeiro','Leblon','Rua Professor Saboia Ribeiro',83)")
+# Save (commit) the changes
+#con.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+#con.close()
