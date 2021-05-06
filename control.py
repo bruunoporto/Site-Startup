d39b66647defcda3c2942651d6863e9bcf53f063
Pedro  Teixeira <pedroteixeir@me.com>
2021-05-05 23:08:43 -0300

    Added login form in user register page

--- control.py
+++ control.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template, request
-from database import usuario, empresa
+from model import usuario, empresa
 
 ##########################################################################
 
