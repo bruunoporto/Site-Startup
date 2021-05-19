b14a4763d888dfa48e0a9df21abeea21d914ef27
Pedro  Teixeira <pedroteixeir@poli.ufrj.br>
2021-05-19 12:28:36 -0300

    POSTS, ..... , IDS

--- app/routes.py
+++ app/routes.py
@@ -105,9 +105,13 @@ def enterprise_page_specific(name):
         for post in Post.query.all():
            if post.id > id_max:
             id_max = post.id+1
-            if id_max == Post.query.filter_by(id = id_max).first().id:
-                id_max = id_max +1
-            else :
+            try:
+                if id_max == Post.query.filter_by(id = id_max).first().id:
+                    id_max = id_max +1
+                    print(id_max)
+                else :
+                    break
+            except:
                 break
         post = Post(id = id_max,body=text, timestamp = datetime.datetime.now().timestamp(), empresa_id=enterpriseS.id, author_id = current_user.id)
         flash("Post Salvo com Sucesso")
